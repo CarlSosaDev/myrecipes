0e9d8448e14bbbc0ebdb41820b4b8a8243ec0c81
Carlos Sosa <20142133@itla.edu.do>
2017-08-24 02:12:33 +0000

    9Cloud

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,27 @@
 class RecipesController < ApplicationController
 
-  def index
+  def index()
+    @recipes = Recipe.all()
   end
-
+  def new()
+    @recipe = Recipe.new()
+    
+  end
+  def create()
+    @chef = Chef.first()
+    @recipe = @chef.recipes.new(recipe_params)
+    if @recipe.save()
+      flash[:notice]="Recipe was successfully created"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
+    end
+  end
+  
+  
+  private
+  def recipe_params
+    
+    params.require(:recipe).permit(:name, :description)
+  end
 end
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -5,4 +5,14 @@ class RecipesTest < ActionDispatch::IntegrationTest
      get recipes_url
      assert_response :success
    end
+   test "reject invalid recipe submissions " do
+      get new_recipe_path
+      assert_template 'recipes/new'
+      assert_no_difference 'Recipe.count' do
+         post recipes_path, params: {recipe: {name:"",description:""}}
+      end
+      assert_template 'recipes/new'
+      assert_select 'h2.panel-title'
+      assert_select 'div.panel-bodyexit'
+   end
 end
