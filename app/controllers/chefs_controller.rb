9a9e5a2ee0f29e70829076e62e6a37526942403e
Carlos Sosa <c.sosa.got@gmail.com>
2017-08-27 22:08:21 -0500

    added chefs to app

---
+++ app/controllers/chefs_controller.rb
@@ -0,0 +1,33 @@
+class ChefsController < ApplicationController
+
+  def new
+    @chef = Chef.new
+  end
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = "Welcome #{@chef.name} to MyRecipes App!"
+      redirect_to chef_path(@chef)
+    else
+      render 'new'
+    end
+  end
+  def show
+    @chef = Chef.find(params[:id])
+  end
+  def edit
+    @chef = Chef.find(params[:id])
+  end
+  def update
+     @chef= Chef.find(params[:id])
+     if @chef.update(chef_params)
+       flash[:success] = "Your acount was updated succesfully"
+       redirect_to @chef
+     else
+       render 'edit'
+     end
+  end
+  def chef_params
+    params.require(:chef).permit(:name,:email,:password,:password_confirmation)
+  end
+end
