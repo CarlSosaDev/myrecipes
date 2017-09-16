f66db6ab6f682898c775309a6adda39f91debd5a
Carlos Sosa <c.sosa.got@gmail.com>
2017-09-15 23:06:50 -0500

    fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_chef
+  helper_method :current_chef, :logged_in?
 
   def current_chef
      @current_chef ||= Chef.find(session[:chef_id]) if session[:chef_id]
