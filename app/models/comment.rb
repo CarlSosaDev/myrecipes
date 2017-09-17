6f7bd98e04db06a5aa7ab2a5ef35035fb005cc1c
Carlos Sosa <c.sosa.got@gmail.com>
2017-09-16 22:22:50 -0500

    Fixed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,12 +7,12 @@ class Comment < ApplicationRecord
     validates :recipe_id, presence: true
     default_scope -> {order(updated_at: :desc)}
 
-
+if Rails.env.production?
 Comment.class_eval do
   _validators[:article]
     .find { |v| v.is_a? ActiveRecord::Validations::PresenceValidator }
     .attributes
     .delete(:article)
 end
-
+end
 end
