61089fc124d29d0b536c90b45401f6cc9309858a
Carlos Sosa <c.sosa.got@gmail.com>
2017-09-23 15:57:53 -0500

    added images

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -74,7 +74,7 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:name, :description, ingredient_ids: [])
+    params.require(:recipe).permit(:name, :description, :image,ingredient_ids: [])
   end
 
   def require_user_like
